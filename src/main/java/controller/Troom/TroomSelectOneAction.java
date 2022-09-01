cb22415720275a477ddd03faae60ecd80b5e96a8
Kchiho <kim@chihoui-MacBookAir.local>
2022-09-01 09:03:17 +0900

    controller 0.0.9
    리뷰 + 댓글 수정

--- src/main/java/controller/Troom/TroomSelectOneAction.java
+++ src/main/java/controller/Troom/TroomSelectOneAction.java
@@ -1,9 +1,14 @@
 package controller.Troom;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.TreviewDAO;
 import DAO.TroomDAO;
+import VO.TreviewSet;
+import VO.TreviewVO;
 import VO.TroomVO;
 import controller.TActionForward;
 import controller.TInterface;
@@ -16,13 +21,18 @@ public class TroomSelectOneAction implements TInterface{
 		
 		TroomDAO trdao=new TroomDAO();
 		TroomVO trvo=new TroomVO();
+		TreviewVO trvVO=new TreviewVO();
+		TreviewDAO trvDAO=new TreviewDAO();
 		
 		String paramTrpk=request.getParameter("trpk");
 		trvo.setTrpk(Integer.parseInt(paramTrpk));
+		trvVO.setTrpk(Integer.parseInt(paramTrpk));
 		
 		TroomVO data=trdao.selectOne(trvo);
-
-		request.setAttribute("data", data);
+		ArrayList<TreviewSet> trdatas = trvDAO.selectAll(trvVO);
+		
+		request.setAttribute("data", data); // 상품상세
+		request.setAttribute("trdatas", trdatas); // 리뷰 + 댓글
 		forward=new TActionForward();
 		forward.setPath("/roomdetail.jsp");
 		forward.setRedirect(false);
