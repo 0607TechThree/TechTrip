21906ff7fd0842ba8cb42b238ca265c41084833c
Kchiho <kim@chihoui-MacBookAir.local>
2022-09-01 14:41:08 +0900

    .

--- src/main/java/controller/Troom/TroomSelectOneAction.java
+++ src/main/java/controller/Troom/TroomSelectOneAction.java
@@ -23,35 +23,37 @@ public class TroomSelectOneAction implements TInterface{
 		HttpSession session = request.getSession();
 		TActionForward forward=null;
 		boolean flag=true;
-		
+
 		TroomDAO trdao=new TroomDAO();
 		TroomVO trvo=new TroomVO();
 		TreviewVO trvVO=new TreviewVO();
 		TreviewDAO trvDAO=new TreviewDAO();
 		TwishVO twVO=new TwishVO();
 		TwishDAO twDAO=new TwishDAO();
-		
+
 		String paramTrpk=request.getParameter("trpk");
-		
+
 		ArrayList<Integer> cart = (ArrayList<Integer>)session.getAttribute("cart");
-		for(int i=0;i<cart.size();i++) {
-			if(cart.get(i) == Integer.parseInt(paramTrpk)) {
-				flag=false;
+		if(cart.size() != 0) {
+			for(int i=0;i<cart.size();i++) {
+				if(cart.get(i) == Integer.parseInt(paramTrpk)) {
+					flag=false;
+				}
 			}
 		}
-		
+
 		trvo.setTrpk(Integer.parseInt(paramTrpk));
 		trvVO.setTrpk(Integer.parseInt(paramTrpk));
 		twVO.setTrpk(Integer.parseInt(paramTrpk));
-		
+
 		TroomVO data=trdao.selectOne(trvo);
 		ArrayList<TwishVO> wishList= twDAO.selectAll(twVO);
 		ArrayList<TreviewSet> trdatas = trvDAO.selectAll(trvVO);
-		
+
 		System.out.println(data);
 		System.out.println(trdatas);
 		System.out.println(wishList);
-		
+
 		request.setAttribute("flag", flag); // 상품상세
 		request.setAttribute("data", data); // 상품상세
 		request.setAttribute("trdatas", trdatas); // 리뷰 + 댓글
